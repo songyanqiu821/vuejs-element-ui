3f34ef04926d0ea80ec837b464d4e5f31f8d7d96
SongYanqiu <279321615@qq.com>
2018-07-08 21:27:58 +0800

    项目初始化配置

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+// 下面注释的这行不要删除  否则会报错
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-})
+});
