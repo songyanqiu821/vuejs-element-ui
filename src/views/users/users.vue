3c5f6c89c5273e38358d468c604989233dbd4105
SongYanqiu <279321615@qq.com>
2018-07-10 21:44:12 +0800

    删除成功

--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -76,7 +76,7 @@
                 label="操作">
                 <template slot-scope="scope">
                 <el-button plain size="mini" type="primary" icon="el-icon-edit" ></el-button>
-                <el-button plain size="mini" type="danger" icon="el-icon-delete" ></el-button>
+                <el-button  @click="handleDelete(scope.row.id)" size="mini" type="danger" icon="el-icon-delete" ></el-button>
                 <el-button plain size="mini" type="success" icon="el-icon-check" ></el-button>
                 </template>
             </el-table-column>
@@ -254,6 +254,38 @@ export default {
           // 用户添加失败
           this.$message.error(msg);
         }
+      });
+    },
+    // 删除
+    // 给删除绑定事件
+    // 提示是否删除
+    // 发送
+    async handleDelete(id) {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async() => {
+        const res = await this.$http.delete(`users/${id}`);
+        const data = res.data;
+        const {meta: {status, msg}} = data;
+        if (status === 200) {
+          // 删除成功 重新加载数据
+          // 默认让其显示第一页
+          this.pagenum = 1;
+          this.loadData();
+          this.$message({
+            type: 'success',
+            message: msg
+          });
+        } else {
+          this.$message.error(msg);
+        }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
       });
     }
   }
